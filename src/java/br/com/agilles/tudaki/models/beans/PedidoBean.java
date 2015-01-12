dc5bb0f7e1506520f3823070137a4aa1be7687ca
Inteligencia <Inteligencia@e178198.pmlimeira.int>
2015-01-12 16:18:47 -0200

    alteracoes feitas, começando página e bean de pedidos

---
+++ src/java/br/com/agilles/tudaki/models/beans/PedidoBean.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.agilles.tudaki.models.beans;
+
+import br.com.agilles.tudaki.models.business.Pedido;
+import java.io.Serializable;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ * *
+ * TUDAKI - SISTEMA DE GERENCIAMENTO PEDIDOS MOBILE (C) - Todos os direitos
+ * reservados
+ *
+ *
+ * @author Jilles Ragonha
+ * @category Web
+ * @copyright Agilles
+ * @updated 06/01/2015
+ *
+ * @description Pedido Bean - Objeto que fará o controle dos pedidos no sistema.
+ *
+ *
+ *
+ */
+@ManagedBean
+@RequestScoped
+public class PedidoBean implements Serializable {
+
+    private Pedido pedido = new Pedido();
+    private Pedido pedidoSelecionado;
+    private List<Pedido> listaPedidos;
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Pedido getPedidoSelecionado() {
+        return pedidoSelecionado;
+    }
+
+    public void setPedidoSelecionado(Pedido pedidoSelecionado) {
+        this.pedidoSelecionado = pedidoSelecionado;
+    }
+
+    public List<Pedido> getListaPedidos() {
+        return listaPedidos;
+    }
+
+    public void setListaPedidos(List<Pedido> listaPedidos) {
+        this.listaPedidos = listaPedidos;
+    }
+
+}
--- src/java/br/com/agilles/tudaki/models/business/Pedido.java
+++ src/java/br/com/agilles/tudaki/models/business/Pedido.java
@@ -3,23 +3,74 @@ package br.com.agilles.tudaki.models.business;
 import java.util.List;
 
 /**
- * TUDAKI - SISTEMA DE GERENCIAMENTO PEDIDOS MOBILE (C) - Todos os direitos reservados 
- * 
- *  
- * @author      Jilles Ragonha
- * @category    Web
- * @copyright   Agilles
- * @updated     06/06/2014
- * 
+ * TUDAKI - SISTEMA DE GERENCIAMENTO PEDIDOS MOBILE (C) - Todos os direitos
+ * reservados
+ *
+ *
+ * @author Jilles Ragonha
+ * @category Web
+ * @copyright Agilles
+ * @updated 06/06/2014
+ *
  * @description Pedido - Objeto que define os atributos de um Pedido.
- * 
- **/
+ *
+ *
+ */
 public class Pedido {
+
     private double numeroPedido;
     private List<Produto> produtosPedido;
     private FormaPagto pagamento;
     private Endereco enderecoEntrega;
     private Cliente cliente;
     private double valorFinal;
-    
+
+    public double getNumeroPedido() {
+        return numeroPedido;
+    }
+
+    public void setNumeroPedido(double numeroPedido) {
+        this.numeroPedido = numeroPedido;
+    }
+
+    public List<Produto> getProdutosPedido() {
+        return produtosPedido;
+    }
+
+    public void setProdutosPedido(List<Produto> produtosPedido) {
+        this.produtosPedido = produtosPedido;
+    }
+
+    public FormaPagto getPagamento() {
+        return pagamento;
+    }
+
+    public void setPagamento(FormaPagto pagamento) {
+        this.pagamento = pagamento;
+    }
+
+    public Endereco getEnderecoEntrega() {
+        return enderecoEntrega;
+    }
+
+    public void setEnderecoEntrega(Endereco enderecoEntrega) {
+        this.enderecoEntrega = enderecoEntrega;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public double getValorFinal() {
+        return valorFinal;
+    }
+
+    public void setValorFinal(double valorFinal) {
+        this.valorFinal = valorFinal;
+    }
+
 }
