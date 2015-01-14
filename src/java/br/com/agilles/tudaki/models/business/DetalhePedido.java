9cb302ea1b34db32551f0a3b8ff97bd8a361e0d6
Inteligencia <Inteligencia@e178198.pmlimeira.int>
2015-01-14 09:42:57 -0200

    Detalhamento dos itens do pedido ... criando a classe detalhepedido

---
+++ src/java/br/com/agilles/tudaki/models/business/DetalhePedido.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.agilles.tudaki.models.business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Inteligencia
+ */
+public class DetalhePedido {
+    private String numeroPedido;
+    private List<Produto> listaProdutos = new ArrayList<>();
+    private double valorFinal;
+    
+    
+    
+}
